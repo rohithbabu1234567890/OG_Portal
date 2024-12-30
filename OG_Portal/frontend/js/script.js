8ec59659af18a6aa0acf1daaaebe7a1b3d8c9277
rohithbabu1234567890 <rohithbabumayakuntla@gmail.com>
2024-12-30 19:25:34 +0530

    Add files via upload

---
+++ OG_Portal/frontend/js/script.js
@@ -0,0 +1,7 @@
+// Display an alert when the page loads
+alert("Welcome to the Online Complaint Grievance Portal!");
+
+// Function to change the background color
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = "lightgreen";
+}
